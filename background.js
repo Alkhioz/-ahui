a0f188623b6ad3022933990c3257b939e5aca413
alkhioz <alemendoza0197@gmail.com>
2020-09-16 18:07:27 -0500

    now is a functional extension

--- background.js
+++ background.js
@@ -1,8 +1,15 @@
-chrome.browserAction.onClicked.addListener(IconClicked);
-function IconClicked(tab)
-{
-	let msg = {
-		txt : "Hello"
-	}
-	chrome.tabs.sendMessage(tab.id,msg);
-}
+console.log("Background running");
+
+function executeScriptsInExistingTabs(){
+    chrome.windows.getAll(null, function(wins) {
+      for (var j = 0; j < wins.length; ++j) {
+        chrome.tabs.getAllInWindow(wins[j].id, function(tabs) {
+          for (var i = 0; i < tabs.length; ++i) {
+            if (tabs[i].url.indexOf("chrome://") != 0) {
+              chrome.tabs.executeScript(tabs[i].id, { file: 'content.js' });
+            }
+          }
+        });
+       }
+     });
+   }
--- content.js
+++ content.js
@@ -1,10 +1,45 @@
-chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage(message,sender,sendresponse)
-{
-	console.log(message.txt);
-	let paragraphs = document.getElementsByTagName("p");
-	for(elt of paragraphs)
-	{
-		elt.style['background-color'] = '#00CED1';
-	}
-}
+var observeDOM = (function(){
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+  
+    return function( obj, callback ){
+      if( !obj || obj.nodeType !== 1 ) return; // validation
+  
+      if( MutationObserver ){
+        // define a new observer
+        var obs = new MutationObserver(function(mutations, observer){
+            callback(mutations);
+        })
+        // have the observer observe foo for changes in children
+        obs.observe( obj, { childList:true, subtree:true });
+      }
+      
+      else if( window.addEventListener ){
+        obj.addEventListener('DOMNodeInserted', callback, false);
+        obj.addEventListener('DOMNodeRemoved', callback, false);
+      }
+    }
+  })();
+  
+  function eventFire(el, etype){
+        if (el.fireEvent) {
+            el.fireEvent('on' + etype);
+        } else {
+            var evObj = document.createEvent('Events');
+            evObj.initEvent(etype, true, false);
+            el.dispatchEvent(evObj);
+        }
+    }
+    observed_element = document.querySelector('body');
+    
+    observeDOM( observed_element, function(m){
+
+      if(!!document.getElementById("skip-button:z")){
+        var container = document.getElementById("skip-button:z");
+        var adbutton = container.getElementsByTagName("BUTTON")[0];
+        eventFire(adbutton, 'click');
+        console.log('you did it');
+        
+      }
+      console.log('DOM change');
+      });
+console.log('funciona aqui');
